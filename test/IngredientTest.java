23de2034cd891db72b72a0e479d752f24745713c
Andy <anikolaj@umich.edu>
2018-11-07 15:45:57 -0500

    commit

--- test/IngredientTest.java
+++ test/IngredientTest.java
@@ -12,16 +12,25 @@ import javamatic.Ingredient;
 class IngredientTest {
 	private static Ingredient ingredient;
 	
+	/**
+	 * Creates Ingredient object for each test case.
+	 */
 	@BeforeEach
 	void setUp() {
 		ingredient = new Ingredient("Cocoa", 0.90);
 	}
 
+	/**
+	 * Tests the Ingredient name returns the correct value.
+	 */
 	@Test
 	void testName() {
 		assertEquals(ingredient.getName(), "Cocoa");
 	}
 
+	/**
+	 * Tests the Ingredient cost returns the correct value.
+	 */
 	@Test
 	void testCost() {
 		assert(ingredient.getCost() == 0.90);
